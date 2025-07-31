7ce1df7bd25af92825e5725f7cec6dfb487b4c91
Ali Ishaq <aliishaqsandho@gmail.com>
2025-07-31 13:53:02 +0500

    added links

--- src/components/global/footer.jsx
+++ src/components/global/footer.jsx
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom';
+import Dashboard from '../pages/dashboard';
 function Footer() {
     return <>
         <div className="">
@@ -5,7 +7,8 @@ function Footer() {
                 <li className="bg-gray-300 p-2 rounded-lg text-sl w-full font-medium">
                     Hello Main
                     <ul className="flex flex-col items-start justify-center m-2 gap-2">
-                        <li className="cursor-pointer p-2 w-full bg-gray-200 rounded-lg transform transition-transform duration-200 hover:scale-101 hover:bg-gray-400 hover:text-white">Hello Bottom</li>
+                        
+                        <li className="cursor-pointer p-2 w-full bg-gray-200 rounded-lg transform transition-transform duration-200 hover:scale-101 hover:bg-gray-400 hover:text-white"><Link to="Dashboard">Hello Bottom</Link></li>
                         <li className="cursor-pointer p-2 w-full bg-gray-200 rounded-lg transform transition-transform duration-200 hover:scale-105 hover:bg-gray-400 hover:text-white">Hello Bottom</li>
                         <li className="cursor-pointer p-2 w-full bg-gray-200 rounded-lg transform transition-transform duration-200 hover:scale-105 hover:bg-gray-400 hover:text-white">Hello Bottom</li>
                         <li className="cursor-pointer p-2 w-full bg-gray-200 rounded-lg transform transition-transform duration-200 hover:scale-105 hover:bg-gray-400 hover:text-white">Hello Bottom</li>
--- src/components/global/sidebar.jsx
+++ src/components/global/sidebar.jsx
@@ -1,105 +1,38 @@
-function Sidebar() {
-    return <>
-        <div className="flex flex-col justify-start h-screen bg-gray-100">
-            <div className="flex border-b border-b-gray-300 h-14">
-                <div className="p-2 bg-gray-600 hover:bg-gray-800 w-1/6 flex item-center justify-center m-2 rounded-lg"><i className="bi bi-basket-fill text-white cursor-pointer"></i></div>
-                <div className="flex flex-col p-1 justify-center">
-                    <div className="text-xs font-bold">TEMAAM PRRACTICE</div>
-                    <div className="text-xs text-gray-600 ">Admin Panel</div>
-                </div>
-            </div>
-            <div className="p-2 flex flex-col gap-2 overflow-auto">
-                <div className="flex flex-col gap-2">
-                    <span className="text-xs m-2">Main Menu</span>
-                    <ul className="flex gap-2 flex-col">
-                        <li className="flex justify-between p-1.5 items-center gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 cursor-pointer">
-                            <div className="flex gap-2">
-                                <span><i class="bi bi-house"></i></span>
-                                <h5>Dashboard</h5>
-                            </div>
-                            <span className="bg-gray-300 rounded-lg text-sl p-1.6 h-5 w-5 flex items-center justify-center">5</span>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                    </ul>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <span className="text-xs m-2">Main Menu</span>
-                    <ul className="flex gap-2 flex-col">
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                    </ul>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <span className="text-xs m-2">Main Menu</span>
-                    <ul className="flex gap-2 flex-col">
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                    </ul>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <span className="text-xs m-2">Main Menu</span>
-                    <ul className="flex gap-2 flex-col">
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
+import { Link } from 'react-router-dom';
 
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                        <li className="flex p-1.5  gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 ">
-                            <span><i class="bi bi-house"></i></span>
-                            <h5>Dashboard</h5>
-                        </li>
-                    </ul>
-                </div>
-                
-            </div>
+function Sidebar() {
+  return (
+    <div className="flex flex-col justify-start h-screen bg-gray-100">
+      <div className="flex border-b border-b-gray-300 h-14">
+        <div className="p-2 bg-gray-600 hover:bg-gray-800 w-1/6 flex items-center justify-center m-2 rounded-lg">
+          <i className="bi bi-basket-fill text-white cursor-pointer"></i>
         </div>
-    </>
+        <div className="flex flex-col p-1 justify-center">
+          <div className="text-xs font-bold">TEMAAM PRACTICE</div>
+          <div className="text-xs text-gray-600">Admin Panel</div>
+        </div>
+      </div>
+
+      <div className="p-2 flex flex-col gap-2 overflow-auto">
+        <span className="text-xs m-2">Main Menu</span>
+        <ul className="flex gap-2 flex-col">
+          <li className="flex justify-between p-1.5 items-center gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 cursor-pointer">
+            <Link to="/dashboard" className="flex gap-2 w-full">
+              <span><i className="bi bi-house"></i></span>
+              <h5>Dashboard</h5>
+            </Link>
+            <span className="bg-gray-300 rounded-lg text-sl p-1.6 h-5 w-5 flex items-center justify-center">5</span>
+          </li>
+          <li className="flex p-1.5 gap-2 text-sl font-medium bg-gray-200 rounded-lg hover:bg-gray-300 cursor-pointer">
+            <Link to="/products" className="flex gap-2 w-full">
+              <span><i className="bi bi-box"></i></span>
+              <h5>Products</h5>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
-export default Sidebar; 
+
+export default Sidebar;
